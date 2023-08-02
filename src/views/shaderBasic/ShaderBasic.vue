e49d6d5814b97f8219c908b172054cbe99f49700
quyinggang <yinggang.qu@gmail.com>
2023-08-02 12:42:55 +0800

    feat: shader basic

--- src/views/shaderBasic/ShaderBasic.vue
+++ src/views/shaderBasic/ShaderBasic.vue
@@ -6,14 +6,6 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 const canvasElementRef = ref(null)
 const containerElementRef = ref(null)
 
-const getMesh = () => {
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xb9d3ff
-  })
-  return new THREE.Mesh(geometry, material)
-}
-
 const createScene = () => {
   // 创建场景
   const scene = new THREE.Scene()
@@ -23,7 +15,7 @@ const createScene = () => {
 const createCamera = (aspect) => {
   // 透视投影摄像机
   const camera = new THREE.PerspectiveCamera(45, aspect, 1, 1000)
-  camera.position.set(10, 10, 10)
+  camera.position.set(0, 2, 6)
   // 设置摄像机方向
   camera.lookAt(0, 0, 0)
   return camera
@@ -46,6 +38,62 @@ const createWebGLRenderer = (canvasElement, width, height) => {
   return renderer
 }
 
+const createMesh = () => {
+  // 逐顶点
+  const vertexShader = `
+    void main() {
+      // 顶点坐标位置
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `
+  // 逐片元
+  const fragmentShader = `
+    void main() {
+      // 片元颜色
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  `
+  const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader
+  })
+  const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material)
+  mesh.position.set(-1, 0, 0)
+  return mesh
+}
+
+const createMesh2 = () => {
+  // 逐顶点
+  const vertexShader = `
+    void main() {
+      // 顶点坐标位置
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `
+  // 逐片元
+  const fragmentShader = `
+    // 全局属性就意味着在每个片元处理时都是相同值
+    uniform vec3 u_color;
+
+    void main() {
+      // 片元颜色
+      gl_FragColor = vec4(u_color, 1.0);
+    }
+  `
+  // 使用自定义颜色
+  const material = new THREE.ShaderMaterial({
+    // 定义uniforms全局变量
+    uniforms: {
+      u_color: { value: new THREE.Color('#9932CC') }
+    },
+    vertexShader,
+    fragmentShader
+  })
+  const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material)
+  mesh.position.set(1, 0, 0)
+  return mesh
+}
+
 onMounted(() => {
   const canvasElement = canvasElementRef.value
   const containerElement = containerElementRef.value
@@ -57,13 +105,10 @@ onMounted(() => {
   const renderer = createWebGLRenderer(canvasElement, width, height)
   const controls = createControls(camera, renderer.domElement)
 
-  // 网格物体
-  const cube = getMesh()
-  scene.add(cube)
-
-  // 坐标轴辅助
-  const axesHelper = new THREE.AxesHelper(5)
-  scene.add(axesHelper)
+  const mesh = createMesh()
+  scene.add(mesh)
+  const mesh2 = createMesh2()
+  scene.add(mesh2)
 
   const render = () => {
     controls.update()
