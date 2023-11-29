674527fbbf7181edd9a44b6d8b0a8b6bdd91d45e
quyinggang <yinggang.qu@gmail.com>
2023-11-29 21:16:04 +0800

    refactor: shadertoy shape

--- src/views/shadertoy/3DShape.vue
+++ src/views/shadertoy/3DShape.vue
@@ -59,17 +59,31 @@ const createPlane = () => {
       return 1.0 - smoothstep(0.0, 0.005, d);
     }
 
-    vec3 cubePoints(vec2 uv, vec2 center) {
+    float drawLine(in vec2 p, in vec3 sv1, in vec3 sv2) {
+      vec2 lineVec = vec2(sv2.x - sv1.x, sv2.y - sv1.y);
+      vec2 pVec = vec2(p.x - sv1.x, p.y - sv1.y);
+      
+      float distToLine = length(cross(vec3(normalize(lineVec), 0.0), vec3(pVec, 0.0)));
+      float distAlongLine = dot(lineVec, pVec)/dot(lineVec, lineVec);
+      
+      float depth = (1.0 - distAlongLine) * sv1.z + distAlongLine * sv2.z;
+      
+      float val = 1.0 - smoothstep(0.0, 0.005, distToLine - 0.005 /depth);
+      val *= step(0.0, distAlongLine) * step(distAlongLine, 1.0);
+      return val;
+    }
+
+    vec3[8] projectCube(vec2 center) {
       // 1X1正方体坐标
       vec3[8] cube;
       cube[0] = vec3(0.0, 0.0, 0.0);
       cube[1] = vec3(1.0, 0.0, 0.0);
-      cube[2] = vec3(0.0, 0.0, 1.0);
-      cube[3] = vec3(1.0, 0.0, 1.0);
+      cube[2] = vec3(1.0, 0.0, 1.0);
+      cube[3] = vec3(0.0, 0.0, 1.0);
       cube[4] = vec3(0.0, 1.0, 0.0);
       cube[5] = vec3(1.0, 1.0, 0.0);
-      cube[6] = vec3(0.0, 1.0, 1.0);
-      cube[7] = vec3(1.0, 1.0, 1.0);
+      cube[6] = vec3(1.0, 1.0, 1.0);
+      cube[7] = vec3(0.0, 1.0, 1.0);
 
       float depth = 4.0;
       vec3[8] screenVertices;
@@ -84,25 +98,59 @@ const createPlane = () => {
         // 深度
         vertex.z -= depth;
 
-        screenVertices[j] = vec3(-vertex.x / vertex.z, -vertex.y / vertex.z, -vertex.z);
+        vec3 screenVertex = vec3(-vertex.x / vertex.z, -vertex.y / vertex.z, -vertex.z);
+        screenVertex.xy += center;
+        screenVertices[j] = screenVertex;
       }
+      return screenVertices;
+    }
+
+    vec3 cubePoints(vec2 uv, vec2 center) {
+      vec3[8] cube = projectCube(center);
 
       float val = 1.0;
       for (int j = 0; j < 8; j++) {
-        vec3 vertex = screenVertices[j];
-        vertex.xy += center;
-        val *= 1.0 - drawPoint(uv, vertex);
+        val *= 1.0 - drawPoint(uv, cube[j]);
       }
       val = 1.0 - val;
-      vec3 vertexColor = vec3(0.0, 1.0, 1.0);
-      vec3 backgroundColor = vec3(0.0);
-      return backgroundColor + val * vertexColor;
+      return val * vec3(0.0, 1.0, 1.0);
+    }
+
+    vec3 cubeLine(vec2 uv, vec2 center) {
+      vec3[8] cube = projectCube(center);
+
+      ivec2[12] edge;
+      edge[0] = ivec2(0, 1);
+      edge[1] = ivec2(0, 3);
+      edge[2] = ivec2(0, 4);
+      edge[3] = ivec2(1, 2);
+      edge[4] = ivec2(1, 5);
+      edge[5] = ivec2(2, 3);
+      edge[6] = ivec2(2, 6);
+      edge[7] = ivec2(3, 7);
+      edge[8] = ivec2(4, 5);
+      edge[9] = ivec2(5, 6);
+      edge[10] = ivec2(6, 7);
+      edge[11] = ivec2(4, 7);
+
+      float val = 1.0;
+      for (int j = 0; j < 12; j++) {
+        ivec2 index = edge[j];
+        vec3 current = cube[index[0]];
+        vec3 next = cube[index[1]];
+        val *= 1.0 - drawLine(uv, current, next);
+      }
+
+      val = 1.0 - val;
+
+      return val * vec3(0.0, 1.0, 1.0);
     }
 
     void mainImage(out vec4 fragColor, in vec2 fragCoord) {
       vec2 uv = (2.0 * fragCoord.xy - iResolution.xy) / min(iResolution.y, iResolution.x);
-      vec3 color = cubePoints(uv, vec2(-1.0, 0.5));
-      fragColor = vec4(color, 1.0);
+      vec3 cubePointColor = cubePoints(uv, vec2(-1.0, 0.5));
+      vec3 cubeLineColor = cubeLine(uv, vec2(0.0, 0.5));
+      fragColor = vec4(cubePointColor + cubeLineColor + cubeBoxColor, 1.0);
     }
 
     void main() {
